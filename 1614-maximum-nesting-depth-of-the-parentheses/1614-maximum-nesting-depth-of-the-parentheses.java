8e2072d1fb3a9a93dd47c0917ac5cea79c73015f
Garule Prabhudas <prabhudas552@gmail.com>
2022-05-31 08:58:27 +0530

    Time: 1 ms (78.69%), Space: 42.5 MB (15.76%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -3,20 +3,16 @@ The depth of any character in the VPS is the ( number of left brackets before it
 */
 class Solution {
     public int maxDepth(String str) {
-        Stack<Character> s = new Stack<>();
         int maxDepth = 0;
-        int length = 0;
+        int leftBrackets = 0;
+        int rightBrackets = 0;
         for(Character c : str.toCharArray()) {
-            if(c == '(') {
-                s.push('(');
-                length++;
-            } else if(c == ')') {
-                s.pop();
-                maxDepth = Math.max(maxDepth, length);
-                length--;
-            } else {
-                maxDepth = Math.max(maxDepth, length);
-            }
+            // 1. get the max depth at current character
+            maxDepth = Math.max(maxDepth, leftBrackets - rightBrackets);
+            
+            // 2. update count
+            if(c == '(') leftBrackets++;
+            if(c == ')') rightBrackets++;
         }
         return maxDepth;
     }
