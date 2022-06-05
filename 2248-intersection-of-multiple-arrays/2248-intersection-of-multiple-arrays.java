1ab72cc038b383e79834033fc1521822ad5e6475
Garule Prabhudas <prabhudas552@gmail.com>
2022-06-05 19:27:59 +0530

    Time: 29 ms (5.42%), Space: 47.2 MB (31.63%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.java
@@ -1,72 +1,63 @@
 class Solution {
     public List<Integer> intersection(int[][] nums) {
-        // sort all individual arrays
-        for(int[] arr : nums) {
-            Arrays.sort(arr);
+        // use priority queue, build priority queue for each array
+        List<PriorityQueue<Integer>> pqList = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++) {
+            PriorityQueue<Integer> q = new PriorityQueue<>();
+            for(int num : nums[i]) {
+                q.add(num);
+            }
+            pqList.add(q);
         }
         
-        // find the intersections
+        // Find intersection and build result
         List<Integer> res = new ArrayList<>();
-        
-        int[] pointers = new int[nums.length];
-        
-        boolean completed = false;
-        
-        while(!isEmpty(pointers, nums)) {
-            // check for intersection
-            if(isIntersection(pointers, nums)) {
-                res.add(nums[0][pointers[0]]);
-                
-                // increment pointers
-                movePointers(pointers);
-            } else {
-                // move pointer with smallest value
-                moveSmallestPointer(pointers, nums);
+        while(!anyPQEmpty(pqList)) {
+            // check if top of pq is intersection
+            int curr = pqList.get(0).peek();
+            boolean isIntersection = true;
+            for(int i = 1; i < pqList.size(); i++) {
+                if(curr != pqList.get(i).peek()) {
+                    // not intersection point
+                    // find min pq and move its head
+                    moveMinPQHead(pqList);
+                    isIntersection = false;
+                    break;
+                }
+            }
+            
+            if(isIntersection) {
+                res.add(curr);
+                moveAllHeads(pqList);
             }
         }
-        
         return res;
     }
     
-    private void movePointers(int []pointers) {
-        for(int i = 0; i < pointers.length; i++) {
-            pointers[i]++;
+    private boolean anyPQEmpty(List<PriorityQueue<Integer>> pqList) {
+       for(PriorityQueue<Integer> p : pqList) {
+            if(p.isEmpty()) return true;
+       }
+       return false; 
+    }
+    
+    private void moveAllHeads(List<PriorityQueue<Integer>> pqList) {
+        for(PriorityQueue<Integer> p : pqList) {
+            p.remove();
         }
     }
     
-    private void moveSmallestPointer(int []pointers, int [][] nums) {
-        int small = nums[0][pointers[0]];
-        int smallIndex = 0;
+    private void moveMinPQHead(List<PriorityQueue<Integer>> pqList) {
+        int minPQHead = 0;
+        int minValue = pqList.get(0).peek();
         
-        for(int i = 1; i < pointers.length; i++) {
-            if(nums[i][pointers[i]] < small) {
-                small = nums[i][pointers[i]];
-                smallIndex = i;
-            }
-        }
-        pointers[smallIndex]++;
-    }
-    
-    private boolean isIntersection(int []pointers, int [][] nums) {
-        // check current value is intersection
-        int curr = nums[0][pointers[0]];
-        
-        for(int i = 1; i < pointers.length; i++) {
-            if(nums[i][pointers[i]] != curr) {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    private boolean isEmpty(int[] pointers, int [][]nums) {
-        // if any array returns end, then no need to find intersection further
-        for(int i = 0; i < pointers.length; i++) {
-            if(pointers[i] >= nums[i].length) {
-                return true;
+        for(int i = 1; i < pqList.size(); i++) {
+            if(pqList.get(i).peek() < minValue) {
+                minValue = pqList.get(i).peek();
+                minPQHead = i;
             }
         }
         
-        return false;
+        pqList.get(minPQHead).remove();
     }
 }
